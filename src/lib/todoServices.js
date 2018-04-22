3cb0634e76b91a2aee02b411636dba0543b29228
Andrew Van Slaars <avanslaars@gmail.com>
2018-04-22 09:44:11 -0400

    Minor formatting for screencast

--- src/lib/todoServices.js
+++ src/lib/todoServices.js
@@ -1,39 +1,36 @@
 const baseUrl = process.env.REACT_APP_BASE_URL
 
 export const getTodos = () => {
-  return fetch(baseUrl)
-    .then(res => res.json())
+  return fetch(baseUrl).then(res => res.json())
 }
 
-export const createTodo = (name) => {
+export const createTodo = name => {
   return fetch(baseUrl, {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({name: name, isComplete: false})
-  })
-    .then(res => res.json())
+    body: JSON.stringify({ name: name, isComplete: false })
+  }).then(res => res.json())
 }
 
-export const updateTodo = (todo) => {
+export const updateTodo = todo => {
   return fetch(`${baseUrl}/${todo.id}`, {
     method: 'PUT',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(todo)
-  })
-    .then(res => res.json())
+  }).then(res => res.json())
 }
 
-export const destroyTodo = (id) => {
+export const destroyTodo = id => {
   return fetch(`${baseUrl}/${id}`, {
     method: 'DELETE',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json'
     }
   })
--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,9 @@
-import {getTodos, createTodo, updateTodo, destroyTodo} from './lib/todoServices'
+import {
+  getTodos,
+  createTodo,
+  updateTodo,
+  destroyTodo
+} from './lib/todoServices'
 
 const initState = {
   todos: [],
@@ -15,63 +20,59 @@ const REMOVE_TODO = 'REMOVE_TODO'
 const SHOW_LOADER = 'SHOW_LOADER'
 const HIDE_LOADER = 'HIDE_LOADER'
 
-export const updateCurrent = (val) => ({type:UPDATE_CURRENT, payload: val})
-export const loadTodos = (todos) => ({type: LOAD_TODOS, payload: todos})
-export const addTodo = (todo) => ({type: ADD_TODO, payload: todo})
-export const replaceTodo = (todo) => ({type: REPLACE_TODO, payload: todo })
-export const removeTodo = (id) => ({type: REMOVE_TODO, payload: id})
-export const showLoader = () => ({type: SHOW_LOADER, payload: true})
-export const hideLoader = () => ({type: HIDE_LOADER, payload: false})
+export const updateCurrent = val => ({ type: UPDATE_CURRENT, payload: val })
+export const loadTodos = todos => ({ type: LOAD_TODOS, payload: todos })
+export const addTodo = todo => ({ type: ADD_TODO, payload: todo })
+export const replaceTodo = todo => ({ type: REPLACE_TODO, payload: todo })
+export const removeTodo = id => ({ type: REMOVE_TODO, payload: id })
+export const showLoader = () => ({ type: SHOW_LOADER, payload: true })
+export const hideLoader = () => ({ type: HIDE_LOADER, payload: false })
 
 export const fetchTodos = () => {
-  return (dispatch) => {
+  return dispatch => {
     dispatch(showLoader())
-    getTodos()
-      .then(todos => {
-        dispatch(loadTodos(todos))
-        dispatch(hideLoader())
-      })
+    getTodos().then(todos => {
+      dispatch(loadTodos(todos))
+      dispatch(hideLoader())
+    })
   }
 }
 
-export const saveTodo = (name) => {
-  return (dispatch) => {
+export const saveTodo = name => {
+  return dispatch => {
     dispatch(showLoader())
-    createTodo(name)
-      .then(res => {
-        dispatch(addTodo(res))
-        dispatch(hideLoader())
-      })
+    createTodo(name).then(res => {
+      dispatch(addTodo(res))
+      dispatch(hideLoader())
+    })
   }
 }
 
-export const toggleTodo = (id) => {
+export const toggleTodo = id => {
   return (dispatch, getState) => {
     dispatch(showLoader())
-    const {todos} = getState()
+    const { todos } = getState()
     const todo = todos.find(t => t.id === id)
-    const toggled = {...todo, isComplete: !todo.isComplete}
-    updateTodo(toggled)
-      .then(res => {
-        dispatch(replaceTodo(res))
-        dispatch(hideLoader())
-      })
+    const toggled = { ...todo, isComplete: !todo.isComplete }
+    updateTodo(toggled).then(res => {
+      dispatch(replaceTodo(res))
+      dispatch(hideLoader())
+    })
   }
 }
 
-export const deleteTodo = (id) => {
-  return (dispatch) => {
+export const deleteTodo = id => {
+  return dispatch => {
     dispatch(showLoader())
-    destroyTodo(id)
-      .then(() => {
-        dispatch(removeTodo(id))
-        dispatch(hideLoader())
-      })
+    destroyTodo(id).then(() => {
+      dispatch(removeTodo(id))
+      dispatch(hideLoader())
+    })
   }
 }
 
 export const getVisibleTodos = (todos, filter) => {
-  switch(filter) {
+  switch (filter) {
     case 'active':
       return todos.filter(t => !t.isComplete)
     case 'completed':
@@ -84,23 +85,30 @@ export const getVisibleTodos = (todos, filter) => {
 export default (state = initState, action) => {
   switch (action.type) {
     case ADD_TODO:
-      return {...state, currentTodo: '', todos: state.todos.concat(action.payload)}
+      return {
+        ...state,
+        currentTodo: '',
+        todos: state.todos.concat(action.payload)
+      }
     case LOAD_TODOS:
-      return {...state, todos: action.payload}
+      return { ...state, todos: action.payload }
     case UPDATE_CURRENT:
-      return {...state, currentTodo: action.payload}
+      return { ...state, currentTodo: action.payload }
     case REPLACE_TODO:
-      return {...state,
-        todos: state.todos
-          .map(t => t.id === action.payload.id ? action.payload : t)
+      return {
+        ...state,
+        todos: state.todos.map(
+          t => (t.id === action.payload.id ? action.payload : t)
+        )
       }
     case REMOVE_TODO:
-      return {...state,
+      return {
+        ...state,
         todos: state.todos.filter(t => t.id !== action.payload)
       }
     case SHOW_LOADER:
     case HIDE_LOADER:
-      return {...state, isLoading: action.payload}
+      return { ...state, isLoading: action.payload }
     default:
       return state
   }
